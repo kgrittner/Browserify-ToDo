bb26d5c062e882be66192adf457f680c89313fc1
Karl Grittner <karlgrittner@gmail.com>
2015-07-15 21:29:38 -0500

    First Check in

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt) {
+    // setup configuration options
+    grunt.initConfig({
+        browserify: {
+            app: {
+                src: 'src/js/app.js',
+                dest: 'dist/js/app.bundle.js',
+                options: {
+                    browserifyOptions: {
+                        debug: true,
+                        transform: 'hbsfy'
+                    }
+                }
+            },
+
+            test: {
+                src: 'spec/**/*.spec.js',
+                dest: 'tmp/spec.bundle.js',
+                options: {
+                    browserifyOptions: {
+                        debug: true,
+                        transform: 'hbsfy',
+                        plugin: 'proxyquireify/plugin'
+                    }
+                }
+            }
+        },
+
+        watch: {
+            app: {
+                files: ['src/js/**/*.js'],
+                tasks: ['browserify'],
+                options: {
+                    livereload: true,
+                    transform: 'hbsfy'
+                }
+            }
+        },
+
+        connect: {
+            app: {
+                options: {
+                    port: 9001,
+                    base: './dist',
+                    middleware: function (connect, options, middlewares) {
+                        middlewares.unshift(require('connect-livereload')());
+                        return middlewares;
+                    }
+                }
+            }
+        },
+
+        jasmine: {
+            app: {
+                options: {
+                    specs: 'tmp/spec.bundle.js'
+                }
+            }
+        }
+
+
+
+    });
+
+    // load additional modules / plugins
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+    // define tasks
+    grunt.registerTask('default', ['browserify']);
+    grunt.registerTask('serve', ['browserify:app', 'connect:app', 'watch:app']);
+    grunt.registerTask('test', ['browserify:test','jasmine']);
+
+
+};
